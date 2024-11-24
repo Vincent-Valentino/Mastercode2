eda93518af5a689af63b2e17494d004687b4b2d2
Vincent-Valentino <valentinolubu2@gmail.com>
2024-11-24 15:07:05 +0700

    production fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          icons: ['@tabler/icons-react'],
-        },
-      },
-    },
-  },
   plugins: [react()],
 })
